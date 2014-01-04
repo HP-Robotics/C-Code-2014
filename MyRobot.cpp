5dbb0f5a871b5f4501621def64e16d202fa7f2bd
Misha Prasolov <mishanet123@gmail.com>
2014-01-04 11:32:19 -0600

    Stripping the Template

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,51 +1,36 @@
 #include "WPILib.h"
 
-/**
- * This is a demo program showing the use of the RobotBase class.
- * The SimpleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- */ 
 class RobotDemo : public SimpleRobot
 {
-	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+	RobotDrive myRobot;
+	Joystick stick;
 
 public:
 	RobotDemo():
-		myRobot(1, 2),	// these must be initialized in the same order
-		stick(1)		// as they are declared above.
+		myRobot(1, 2),
+		stick(1)
 	{
 		myRobot.SetExpiration(0.1);
 	}
-
-	/**
-	 * Drive left & right motors for 2 seconds then stop
-	 */
+	
 	void Autonomous()
 	{
 		myRobot.SetSafetyEnabled(false);
-		myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
+		myRobot.Drive(-0.5, 0.0);
+		Wait(2.0);
+		myRobot.Drive(0.0, 0.0);
 	}
 
-	/**
-	 * Runs the motors with arcade steering. 
-	 */
 	void OperatorControl()
 	{
 		myRobot.SetSafetyEnabled(true);
 		while (IsOperatorControl())
 		{
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
-			Wait(0.005);				// wait for a motor update time
+			myRobot.ArcadeDrive(stick);
+			Wait(0.005);
 		}
 	}
-	
-	/**
-	 * Runs during test mode
-	 */
+
 	void Test() {
 
 	}
